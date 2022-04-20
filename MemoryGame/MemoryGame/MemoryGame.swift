cabe9260133c377e6a9537e4875eacc72073eee6
Daniel de Andrade Souza <daniel.android.andrade@gmail.com>
2022-04-20 12:17:33 -0300

    adicionei um opcional na função índex e usei o método firstIndex where na função choose pra conferir o índex correto que foi passado

--- MemoryGame/MemoryGame/MemoryGame.swift
+++ MemoryGame/MemoryGame/MemoryGame.swift
@@ -12,18 +12,19 @@ struct MemoryGame<CardContent> {
     private(set) var cards: Array<Card>
     
     mutating func choose(_ card: Card){
-        let chosenIndex = index(of: card)
-        cards[chosenIndex].isFaceUp.toggle()
+        if let chosenIndex = cards.firstIndex(where: {$0.id == card.id}){
+            cards[chosenIndex].isFaceUp.toggle()
+        }
         print("chosenCard = \(cards)")
     }
     
-    func index(of card: Card) -> Int {
+    func index(of card: Card) -> Int? {
         for index in 0..<cards.count {
             if cards[index].id == card.id {
                 return index
             }
         }
-        return 0
+        return nil
     }
     
     init(numberOfPairOfCards: Int, createCardContent: (Int) -> CardContent){
@@ -38,7 +39,7 @@ struct MemoryGame<CardContent> {
     
     struct Card: Identifiable{
         
-        var isFaceUp: Bool = true
+        var isFaceUp: Bool = false
         var isMatched: Bool = false
         var content: CardContent
         var id: Int
